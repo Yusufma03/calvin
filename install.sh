fd954034eca8891fbfd14376764af7167598c763
xiao.ma <xiao.ma@bytedance.com>
2024-09-05 10:59:31 +0800

    fix requirements

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wheel is never depended on, but always needed. MulticoreTSNE requires lower CMake version
-pip install wheel cmake==3.18.4
+pip install wheel cmake==3.18.4 setuptools==57.5.0
 
 cd calvin_env/tacto
 pip install -e .
